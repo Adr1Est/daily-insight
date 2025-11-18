73a00597c0324a888dcd6021c54a7901d1fcbd62
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-11-18 12:36:29 +0100

    clean up and info component

---
+++ app/ui/info/Info.tsx
@@ -0,0 +1,8 @@
+export default function Info(){
+  return(
+    <div className="flex flex-col gap-1">
+      <h1>Daily bug</h1>
+      <p>Cada día un bug famoso del mundo de los videojuegos</p>
+    </div>
+  )
+}
