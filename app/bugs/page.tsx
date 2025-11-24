c438fd06c8383594740177649d0627218023bef3
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-11-24 11:20:08 +0100

    bugs route

---
+++ app/bugs/page.tsx
@@ -0,0 +1,5 @@
+export default function BugsList(){
+  return(
+    <p>Hola</p>
+  )
+}
--- app/ui/footer/FooterMain.tsx
+++ app/ui/footer/FooterMain.tsx
@@ -1,32 +1,45 @@
 import { Github, Bot, Linkedin, SquareChartGantt } from 'lucide-react'
+import Link from 'next/link';
 
 export default function FooterMain(){
   return(
     <div className="flex flex-col justify-center items-center text-xs opacity-50">
       
-      <div className='flex flex-row gap-3'>
+      <div className='flex flex-col sm:flex-row gap-3'>
         <p>&copy; 2025 Desarrollado por Adrián Estévez</p>
-        <a 
-          className='cursor-pointer hover:text-orange-300'
-          href='https://github.com/Adr1Est'
-          target='_blank'
-        >
-          <Github size={15}/>
-        </a>
-        <a 
-          className='cursor-pointer hover:text-orange-300'
-          href='https://www.linkedin.com/in/adrianestevezsalamanca/'
-          target='_blank'
-        >
-          <Linkedin size={15}/>
-        </a>
-        <a 
-          className='cursor-pointer hover:text-orange-300'
-          href='https://www.adrianestevezs.dev'
-          target='_blank'
-        >
-          <SquareChartGantt size={15}/>
-        </a>
+
+        <div className='flex flex-row gap-1'>
+          <a 
+            className='cursor-pointer hover:text-orange-300'
+            href='https://github.com/Adr1Est'
+            target='_blank'
+          >
+            <Github size={15}/>
+          </a>
+          <a 
+            className='cursor-pointer hover:text-orange-300'
+            href='https://www.linkedin.com/in/adrianestevezsalamanca/'
+            target='_blank'
+          >
+            <Linkedin size={15}/>
+          </a>
+          <a 
+            className='cursor-pointer hover:text-orange-300'
+            href='https://www.adrianestevezs.dev'
+            target='_blank'
+          >
+            <SquareChartGantt size={15}/>
+          </a>
+
+          <Link
+            href={"/bugs"}
+            className='hover:text-orange-300'
+          >
+            <p>Bugs</p>
+          </Link>
+
+        </div>
+        
       </div>
 
       <div className='flex flex-row gap-1'>
