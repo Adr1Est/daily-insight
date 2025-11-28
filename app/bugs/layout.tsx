1f328d11a93f64d58e18c1f7fe88a929bcfeef2d
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-11-28 16:22:33 +0100

    fixed height bug and smartphone view for bug route

--- app/bugs/layout.tsx
+++ app/bugs/layout.tsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 export default function BugsMain({ children }: { children: React.ReactNode }){
   return(
     <div className="min-h-dvh min-w-dvw flex flex-col justify-center items-center gap-3">
-      <div className="flex items-center lg:justify-center max-h-200 overflow-y-auto w-full overflow-x-auto">
+      <div className="flex lg:justify-center max-h-150 overflow-y-auto w-full overflow-x-auto">
         {children}
       </div>
       <Link
         href={'/'}
-        className="hover:text-orange-300"
+        className="p-2 bg-green-800 rounded-full hover:text-orange-300 fixed bottom-20 right-10 sm:relative opacity-80"
       >
         <Home size={18}/>
       </Link>
