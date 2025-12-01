e128ca449966492106335e6151c9b51c81f23078
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-12-01 12:27:08 +0100

    búsqueda por juego, plataforma o bug

--- app/bugs/page.tsx
+++ app/bugs/page.tsx
@@ -2,13 +2,28 @@
 
 import styles from '@/app/bugs/bugs.module.css';
 import CustomTableRow from '../ui/bugs/CustomTableRow';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 import { BugType } from '@/app/types/bug.types';
 
 export default function BugsList(){
 
   const [bugList, setBugList] = useState<BugType[]>([]);
   const [error, setError] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleFilter = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setFilter(value);
+    console.log(value);
+  }
+
+  const filteredBugList = useMemo(() => {
+    if(!filter) return bugList;
+    return bugList.filter(bug => (bug.game.toLowerCase().includes(filter.toLowerCase()) 
+      || bug.data.toLowerCase().includes(filter.toLowerCase())
+      || bug.platform.toLowerCase().includes(filter.toLowerCase())
+    ));
+  }, [filter, bugList]);
 
   useEffect(() => {
     fetch('/api/getBugs')
@@ -24,37 +39,49 @@ export default function BugsList(){
   }, []);
 
   return(
-    <table className={`${styles.table} `}>
-      <thead className='text-xl'>
-        <tr>
-          <th className={styles.th}>Fecha</th>
-          <th className={styles.th}>Videojuego</th>
-          <th className={styles.th}>Plataforma</th>
-          <th className={styles.th}>Bug</th>
-          <th className={styles.th}>Año</th>
-        </tr>
-      </thead>
-      <tbody>
-        {bugList && !error
-          ? bugList.map((bug: BugType) => (
-              <CustomTableRow
-                key={bug.id}
-                date={`${bug.day} de ${bug.month} de ${bug.year}`}
-                game={bug.game}
-                platform={bug.platform}
-                bug={bug.data}
-                bugYear={bug.bugYear}
-              />
-            ))
-          : (<CustomTableRow
-                date="Sin datos"
-                game="Sin datos"
-                platform="Sin datos"
-                bug="Sin datos"
-                bugYear={0}
-              />)
-        }
-      </tbody>
-    </table>
+    <div className='relative'>
+
+      <input 
+        type="text" 
+        id="searchFilterInput" 
+        className='w-full border mb-1 p-1 sticky top-0 bg-emerald-950' 
+        placeholder='Busca por bug...' 
+        onChange={handleFilter}
+        value={filter}
+      />
+
+      <table className={`${styles.table} `}>
+        <thead className='text-xl'>
+          <tr>
+            <th className={styles.th}>Fecha</th>
+            <th className={styles.th}>Videojuego</th>
+            <th className={styles.th}>Plataforma</th>
+            <th className={styles.th}>Bug</th>
+            <th className={styles.th}>Año</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredBugList && !error
+            ? filteredBugList.map((bug: BugType) => (
+                <CustomTableRow
+                  key={bug.id}
+                  date={`${bug.day} de ${bug.month} de ${bug.year}`}
+                  game={bug.game}
+                  platform={bug.platform}
+                  bug={bug.data}
+                  bugYear={bug.bugYear}
+                />
+              ))
+            : (<CustomTableRow
+                  date="Sin datos"
+                  game="Sin datos"
+                  platform="Sin datos"
+                  bug="Sin datos"
+                  bugYear={0}
+                />)
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
