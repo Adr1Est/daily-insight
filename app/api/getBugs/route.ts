c6fbfce5d14ba8bf47666555e58fa8b2ed0117d6
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-12-01 11:31:23 +0100

    getBugs endpoint

---
+++ app/api/getBugs/route.ts
@@ -0,0 +1,26 @@
+import prisma from "@/lib/prisma";
+
+export async function GET(req: Request){
+
+  const authHeader = req.headers.get("authorization");
+  if(authHeader !== `Bearer ${process.env.INTERNAL_TOKEN}`){
+    return Response.json({error: "Unauthorized"}, {status: 401});
+  }
+
+  try {
+    
+    const data = await prisma.insight.findMany({
+      orderBy: {
+        id: 'desc'
+      }
+    });
+
+    return Response.json(data);
+
+  } catch (error) {
+    
+    return Response.json({error}, {status: 500});
+
+  }
+
+}
